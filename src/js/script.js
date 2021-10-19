a6b5c0464e07f45f1a24904f7f456ce5340b4c31
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-10-19 22:49:42 +0300

    add more features(menu fade animation/ reveal sections/ make navbar sticky > using intersection observer API) and change whole style

--- src/js/script.js
+++ src/js/script.js
@@ -2,27 +2,25 @@
 
 const section_1 = document.querySelector('.section-1-img');
 const nav = document.querySelector('.nav');
-const btn1 = document.querySelector('.btn');
 
 const headerSticky = function (entries) {
   !entries[0].isIntersecting
     ? nav.classList.add('sticky')
     : nav.classList.remove('sticky');
 };
+
 const section_1_observer = new IntersectionObserver(headerSticky, {
   root: null,
   threshold: 0,
   rootMargin: `-${nav.getBoundingClientRect().height}px`,
 });
-
 section_1_observer.observe(section_1);
 
 const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+  if (!entries[0].isIntersecting) return;
+  entries[0].target.classList.remove('section--hidden');
+  observer.unobserve(entries[0].target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -35,6 +33,16 @@ allSections.forEach(section => {
   section.classList.add('section--hidden');
 });
 
-btn1.addEventListener('click', () => {
-  document.querySelector('#html').scrollIntoView({ behavior: 'smooth' });
-});
+function handler(e) {
+  if (e.target.classList.contains('nav__link')) {
+    e.target
+      .closest('nav')
+      .querySelectorAll('.nav__link')
+      .forEach(el => {
+        if (el !== e.target) el.style.opacity = this;
+      });
+  }
+}
+
+nav.addEventListener('mouseover', handler.bind(0.5));
+nav.addEventListener('mouseout', handler.bind(1));
